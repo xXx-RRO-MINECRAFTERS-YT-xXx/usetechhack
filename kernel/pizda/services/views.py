c710fcd0194b80c4f4b3da3c399042ea751ad4fd
Ghost-ate-your-brains <ghost-brake-your-home@protonmail.com>
2023-09-30 17:15:26 +0300

    Added Keycloak lib

--- kernel/pizda/services/views.py
+++ kernel/pizda/services/views.py
@@ -4,3 +4,6 @@ from django.template import loader
 def api(request, service):
     respone = "You requested " + service
     return HttpResponse(respone)
+
+def password_reset(request):
+    return HttpResponse("BAZA")
